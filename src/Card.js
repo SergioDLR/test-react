4b232318d8b28b303dde2c20738a9f6ff334e9dd
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2022-04-08 16:15:29 -0300

    Final push

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,15 @@
 import React from "react";
-const Card = () => {
- 
-
-  return  (
+const Card = ({ name, lastName, email, phone }) => {
+  return (
     <div className="col-span-4 px-3 py-2 shadow-md">
       <img src="imagen large" alt="" className="mx-auto" />
       <h1 className=" my-2 text-lg font-semibold">
-       Name and last name
+        Nombre: {name} Apellido: {lastName}
       </h1>
-      <p className=" my-1 text-gray-600">email</p>
-      <p className="my-1 text-gray-600">phone</p>
+      <p className=" my-1 text-gray-600">Direccion de correo: {email}</p>
+      <p className="my-1 text-gray-600">Numero de telefono: {phone}</p>
     </div>
-  )
+  );
 };
 
 export default Card;
--- src/Test.js
+++ src/Test.js
@@ -1,12 +1,28 @@
-import React from "react";
-// import necesary info
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
 
 const Test = () => {
-  return (
-    <div className="grid grid-cols-12 gap-x-4 gap-y-8 p-2">
-      {/* Render every card adding its data */}
-    </div>
-  );
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    //Async function realiza fetch y lo guarda en el hook userData
+    async function getUsers() {
+      let users = await fetch("https://randomuser.me/api/?results=9&exc=gender,location,login,dob,registered,id");
+      let data = await users.json();
+      setUserData(data.results);
+    }
+    getUsers();
+  }, []);
+
+  const RenderUsers = () => {
+    //Mapea los elementos devueltos por getUsers() en el componente card
+    if (userData.length <= 0) return <h1>Loading...</h1>;
+    else
+      return userData.map((user, index) => (
+        <Card key={index} name={user.name.first} lastName={user.name.last} phone={user.phone} email={user.email} />
+      ));
+  };
+
+  return <div className="grid grid-cols-12 gap-x-4 gap-y-8 p-2">{<RenderUsers />}</div>;
 };
 
 export default Test;
